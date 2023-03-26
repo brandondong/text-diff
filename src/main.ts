2ab041304c29f6d62ffb94aaa98819a3a902e101
Brandon <brandondong96@gmail.com>
2023-03-26 10:07:07 -0700

    Automatically close details panel

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { parse, html } from 'diff2html';
 
 const diffShadowHost = document.getElementById("diff-shadow-host")!;
 
+const detailsForm = document.getElementById("details-form")!;
 const compareForm = document.getElementById("compare-form")!;
 const leftInput = document.getElementById("left-input") as HTMLTextAreaElement;
 const rightInput = document.getElementById("right-input") as HTMLTextAreaElement;
@@ -19,9 +20,13 @@ compareForm.addEventListener("submit", e => {
 
     const diff = createPatch("file_name", leftText, rightText);
     const [diffJson] = parse(diff);
+    // Shadow DOM to stop tailwind reset from applying to diff UI library.
     let shadowDom = initializeShadowDom(diffShadowHost, [diffStyles, diffCustomStyles]);
     const diffHtml = html([diffJson], { outputFormat: "side-by-side", diffStyle: "char" });
     shadowDom.innerHTML = diffHtml;
+
+    // Automatically close the details panel.
+    detailsForm.removeAttribute("open");
 });
 
 function initializeShadowDom(host: HTMLElement, styles: string[]) {
